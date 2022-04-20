a83b06a37e3c213a7e451baf37e5338eb0f82b99
Marcos Avila <marcosa31@gmail.com>
2022-04-19 22:03:50 -0500

    Retrieve text from textview

--- HelloKotlin/app/src/main/java/com/avila/apps/courses/hellokotlin/MainActivity.kt
+++ HelloKotlin/app/src/main/java/com/avila/apps/courses/hellokotlin/MainActivity.kt
@@ -2,10 +2,17 @@ package com.avila.apps.courses.hellokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var tvText = findViewById<TextView>(R.id.textView).text.toString()
+        
+
+
     }
 }

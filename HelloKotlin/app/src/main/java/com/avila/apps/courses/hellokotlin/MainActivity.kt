01440d15a2c36bc5f54fdedb941d8bda17783b90
Marcos Avila <marcosa31@gmail.com>
2022-04-19 22:10:27 -0500

    Configured TextToSpeech
    Validated if the service is available

--- HelloKotlin/app/src/main/java/com/avila/apps/courses/hellokotlin/MainActivity.kt
+++ HelloKotlin/app/src/main/java/com/avila/apps/courses/hellokotlin/MainActivity.kt
@@ -2,17 +2,29 @@ package com.avila.apps.courses.hellokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    var tts: TextToSpeech? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var tvText = findViewById<TextView>(R.id.textView).text.toString()
-        
+        tts = TextToSpeech(this, this)
 
+        var tvText = findViewById<TextView>(R.id.textView).text.toString()
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS){
+            findViewById<TextView>(R.id.textView).text = "Listo!"
+        } else {
+            findViewById<TextView>(R.id.textView).text = "No disponible :("
+        }
 
     }
 }

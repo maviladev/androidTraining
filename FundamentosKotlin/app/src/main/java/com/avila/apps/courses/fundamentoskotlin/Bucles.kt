5cf2c8e47dbd4a05200b955e8d2f76e97e094f27
Marcos Avila <marcosa31@gmail.com>
2022-04-20 23:30:39 -0500

    When statement

--- FundamentosKotlin/app/src/main/java/com/avila/apps/courses/fundamentoskotlin/Bucles.kt
+++ FundamentosKotlin/app/src/main/java/com/avila/apps/courses/fundamentoskotlin/Bucles.kt
@@ -3,7 +3,7 @@ package com.avila.apps.courses.fundamentoskotlin
 fun main() {
     newTopic("Bucles")
     showPersons("Angel", "Mary", "Fany")
-    showPersons("Tony", "Julito")
+    showPersons("Tony", "Julito", "Pepe", "Taka")
 }
 
 fun showPersons(p1: String, p2: String, p3:String){
@@ -19,7 +19,16 @@ fun showPersons(vararg persons: String) {
     newTopic("While")
     var index = 0
     while (index < persons.size){
+        if (persons[index] == "Julito") println("Es Julito!")
         println(persons[index])
         index++
     }
+
+    newTopic("When")
+    index = (0..persons.size-1).random()
+    when(persons[index]){
+        "Tony" -> println("Es tony!")
+        "Julito" -> println("Es Julito!")
+        else -> println(persons[index])
+    } 
 }

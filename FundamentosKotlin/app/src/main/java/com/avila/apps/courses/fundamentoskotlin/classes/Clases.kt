fe1b751a98a030b423c33ea2dcaa22ff8ccff66e
Marcos Avila <marcosa31@gmail.com>
2022-04-21 17:43:49 -0500

    Scope functions With - Apply

--- FundamentosKotlin/app/src/main/java/com/avila/apps/courses/fundamentoskotlin/classes/Clases.kt
+++ FundamentosKotlin/app/src/main/java/com/avila/apps/courses/fundamentoskotlin/classes/Clases.kt
@@ -22,7 +22,7 @@ fun main() {
 
     newTopic("Data classes")
     val myUser = User(1,"Marcos","Avila", Group.FRIEND.ordinal)
-    val bro = myUser.copy(0, "Ivan")
+    val bro = myUser.copy(0, "Ivan", "Kepler", Group.FAMILY.ordinal)
     val workPal = myUser.copy(3, "Peter", "Chill", Group.WORK.ordinal)
 
     println(myUser)
@@ -30,4 +30,20 @@ fun main() {
     println(workPal)
 
     println(myUser.component3())
+
+    newTopic("Funciones de alcance")
+    with(smartPhone){
+        println("Privado ? $isPrivate")
+        call()
+    }
+
+    workPal.group = Group.WORK.ordinal
+    workPal.name = "Juan"
+    workPal.lastName = "Tellez"
+    workPal.apply {
+        group = Group.FAMILY.ordinal
+        name = "Carlos"
+        lastName = "Perez"
+    }
+    println(workPal)
 }

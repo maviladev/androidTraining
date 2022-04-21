abbc544a78e3c2fd8baf66bc6cfb4e0a8420be0c
Marcos Avila <marcosa31@gmail.com>
2022-04-20 22:16:27 -0500

    Constants and variables

--- FundamentosKotlin/app/src/main/java/com/avila/apps/courses/fundamentoskotlin/Principal.kt
+++ FundamentosKotlin/app/src/main/java/com/avila/apps/courses/fundamentoskotlin/Principal.kt
@@ -2,6 +2,12 @@ package com.avila.apps.courses.fundamentoskotlin
 
 fun main(){
     newTopic("Hola Kotlin v3!")
+
+    newTopic("Variables y Constantes")
+    val a = "hola constantes"
+    println(a)
+    var b = 2
+    println("b = $b")
 }
 
 fun newTopic(topic: String){

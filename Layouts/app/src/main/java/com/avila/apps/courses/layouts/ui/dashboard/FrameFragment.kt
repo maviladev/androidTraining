528623cb7e57a054f06fc16257eabadb3bbcfe84
Marcos Avila <marcosa31@gmail.com>
2022-04-21 23:05:22 -0500

    FrameLayout
    Views positioning behaviour

--- Layouts/app/src/main/java/com/avila/apps/courses/layouts/ui/dashboard/FrameFragment.kt
+++ Layouts/app/src/main/java/com/avila/apps/courses/layouts/ui/dashboard/FrameFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.avila.apps.courses.layouts.databinding.FragmentDashboardBinding
+import com.avila.apps.courses.layouts.databinding.FragmentFrameBinding
 
-class DashboardFragment : Fragment() {
+class FrameFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentFrameBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class DashboardFragment : Fragment() {
         val dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentFrameBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard

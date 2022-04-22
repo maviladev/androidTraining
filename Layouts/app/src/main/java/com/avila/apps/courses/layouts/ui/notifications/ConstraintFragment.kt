bf34c542ecb8552c2791a9c3c7c3250389dbc24c
Marcos Avila <marcosa31@gmail.com>
2022-04-21 23:30:29 -0500

    Refactor Notifications Constraint

--- Layouts/app/src/main/java/com/avila/apps/courses/layouts/ui/notifications/ConstraintFragment.kt
+++ Layouts/app/src/main/java/com/avila/apps/courses/layouts/ui/notifications/ConstraintFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.avila.apps.courses.layouts.databinding.FragmentNotificationsBinding
+import com.avila.apps.courses.layouts.databinding.FragmentConstraintBinding
 
-class NotificationsFragment : Fragment() {
+class ConstraintFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentConstraintBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,9 +23,9 @@ class NotificationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+            ViewModelProvider(this).get(ConstraintViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentConstraintBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications

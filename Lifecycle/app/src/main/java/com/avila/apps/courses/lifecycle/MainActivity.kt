b1a696896fc55055e4cf6ebeb8ec922d3a6cad2c
Marcos Avila <marcosa31@gmail.com>
2022-04-23 14:37:08 -0500

    Events CSRPSRsD

--- Lifecycle/app/src/main/java/com/avila/apps/courses/lifecycle/MainActivity.kt
+++ Lifecycle/app/src/main/java/com/avila/apps/courses/lifecycle/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.avila.apps.courses.lifecycle
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "LIFECYCLE-MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i(TAG, "onCreate")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy")
+    }
+
+
 }

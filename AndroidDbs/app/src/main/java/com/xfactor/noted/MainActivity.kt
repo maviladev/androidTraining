0145092e73baf653e9386fb2a18aa53e240eb4ef
Marcos Avila <marcosa31@gmail.com>
2022-04-25 23:43:07 -0500

    CRUD operations definition of List entity

--- AndroidDbs/app/src/main/java/com/xfactor/noted/MainActivity.kt
+++ AndroidDbs/app/src/main/java/com/xfactor/noted/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.xfactor.noted
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.room.Room
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.xfactor.noted.database.AppDatabase
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "noted-db").allowMainThreadQueries().build()
 
         // Setting ActionBar logo
         supportActionBar?.setDisplayShowHomeEnabled(true)
@@ -28,6 +33,18 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+        val listDAO = db.listDAO()
+        val lists: List<com.xfactor.noted.database.List> = listDAO.getAll()
+
+        Log.e("Lists", lists.toString())
+
+        listDAO.insertAll(com.xfactor.noted.database.List(lists.last().uid+1,"Test List"))
+
+        Log.e("Lists", listDAO.getAll().toString())
+
+        listDAO.delete(lists[0])
+
+        Log.e("Lists", listDAO.getAll().toString())
     }
 }
 
---
+++ AndroidDbs/app/src/main/java/com/xfactor/noted/database/List.kt
@@ -0,0 +1,17 @@
+package com.xfactor.noted.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.xfactor.noted.ListItem
+
+@Entity
+data class List (
+
+    @PrimaryKey
+    val uid:Int,
+    @ColumnInfo(name="title")
+    val title: String
+    //@ColumnInfo(name="elements") val elements: MutableList<ListItem>
+
+)
